c7cddac0028bfad84c00d78936542c245e6d4be5
bguiz <brendan.graetz@gmail.com>
2014-08-09 00:02:10 +1000

    =BG= support GET'ing multiple persons at once, plus tests for utility functions

--- nickel-postgres/src/main.rs
+++ nickel-postgres/src/main.rs
@@ -13,8 +13,9 @@ use postgres::pool::{ PostgresConnectionPool, PooledPostgresConnection };
 
 use serialize::json;
 
-use nickel_postgres::PostgresMiddleware;
-use models::{ ServerMessage, Person, PersonByPost, PersonByPut };
+use nickel_postgres::{ PostgresMiddleware, PostgresRequestExtensions };
+use models::{ ServerMessage, GetPersonsMessage };
+use models::{Person, PersonByPost, PersonByPut };
 
 mod models;
 
@@ -27,14 +28,14 @@ fn main() {
     initialise_db_tables(postgres_middleware.pool.clone());
     server.utilize(postgres_middleware);
     server.utilize(Nickel::json_body_parser());
+    //TODO cannot use static files middleware because of middleware chaining bug
+    //See: https://github.com/nickel-org/nickel.rs/issues/59
     server.utilize(Nickel::static_files("client/dist/"));
 
     //Routing
-    //TODO switch to proper RESTful HTTP verbs
-    server.get("/api/person/:id", get_person);
+    server.get("/api/person/:ids", get_persons);
     server.post("/api/person", post_person);
     server.put("/api/person", put_person);
-    //NOTE See https://github.com/nickel-org/nickel.rs/pull/51
     server.delete("/api/person/:id", delete_person);
 
     println!("Listening on port {}", port);
@@ -43,10 +44,21 @@ fn main() {
 
 //initialise database tables, if has not already been done
 fn initialise_db_tables (db_pool_instance: PostgresConnectionPool) {
-    db_pool_instance.get_connection().execute("CREATE TABLE IF NOT EXISTS person (
+    let db_conn = db_pool_instance.get_connection();
+    db_conn.execute("CREATE TABLE IF NOT EXISTS person (
             id          SERIAL PRIMARY KEY,
             name        VARCHAR NOT NULL,
             created     TIMESTAMP NOT NULL
+        )", []).unwrap();
+    db_conn.execute("CREATE TABLE IF NOT EXISTS post (
+            id          SERIAL PRIMARY KEY,
+            title       VARCHAR NOT NULL,
+            text        VARCHAR NOT NULL
+        )", []).unwrap();
+    db_conn.execute("CREATE TABLE IF NOT EXISTS comment (
+            id          SERIAL PRIMARY KEY,
+            text        VARCHAR NOT NULL,
+            post_id  SERIAL REFERENCES post (id)
         )", []).unwrap();
 }
 
@@ -59,17 +71,107 @@ fn get_sole_iterable<T, I: Iterator<T>>(iter: &mut I) -> (Option<T>, uint) {
     }
 }
 
+#[test]
+fn test_get_sole_iterable() {
+    let input: Vec<int> = vec![1 as int];
+    let (val, count) = get_sole_iterable(&mut input.iter());
+    assert_eq!(count, 1);
+    assert_eq!(*(val.unwrap()), 1);
+
+    let input: Vec<int> = vec![1 as int, 2 as int];
+    let (val, count) = get_sole_iterable(&mut input.iter());
+    assert_eq!(count, 2);
+    match val {
+        None => { assert!(true); },
+        Some(_) => { assert!(false); }
+    }
+
+    let input: Vec<int> = vec![];
+    let (val, count) = get_sole_iterable(&mut input.iter());
+    assert_eq!(count, 0);
+    match val {
+        None => { assert!(true); },
+        Some(_) => { assert!(false); }
+    }
+}
+
 fn set_response_content_type_json(response: &mut Response) {
-    let has_set_content_type: bool = match response.origin.headers.content_type {
-        None => false,
-        Some(_) => true,
-    };
-    if !has_set_content_type {
-        response.origin.headers.content_type = Some(http::headers::content_type::MediaType {
-            type_: String::from_str("application"),
-            subtype: String::from_str("json"),
-            parameters: Vec::new()
-        });
+    response.origin.headers.content_type =
+        Some(response.origin.headers.content_type.clone().unwrap_or(
+            http::headers::content_type::MediaType {
+                type_: String::from_str("application"),
+                subtype: String::from_str("json"),
+                parameters: Vec::new()
+            })
+        );
+}
+
+//TODO find way to create a mock Response object for testing
+//#[test]
+// fn test_set_response_content_type_json() {
+//     let mut resp = Response::new();
+//     set_response_content_type_json(&mut resp);
+// }
+
+fn get_comma_seperated_ids(input: &str) -> Vec<i32> {
+    let strs = input.as_slice().split_str(",");
+    let ids: Vec<i32> = strs.filter_map(from_str::<i32>).collect();
+    ids
+}
+
+#[test]
+fn test_get_comma_separated_ids() {
+    assert_eq!(get_comma_seperated_ids("123,4,56"), vec![123,4,56]);
+    assert_eq!(get_comma_seperated_ids("1111"), vec![1111]);
+    //negative numbers are allowed
+    assert_eq!(get_comma_seperated_ids("123,-5,56"), vec![123,-5,56]);
+    //non-numbers are not allowed
+    assert_eq!(get_comma_seperated_ids("123,f,56"), vec![123,56]);
+    //whitespace is not allowed
+    assert_eq!(get_comma_seperated_ids("123, 4,56"), vec![123,56]);
+}
+
+fn get_persons (req: & Request, response: &mut Response) {
+    set_response_content_type_json(response);
+    //TODO find a less verbose way to extract an i32 parameter from request
+    let idsStr: &String = (req.params.get(&"ids".to_string()));
+    let ids = get_comma_seperated_ids(idsStr.as_slice());
+    println!("get_persons 1 ids={}", ids);
+
+    let db_conn = req.db_conn();
+    let stmt = db_conn.prepare(
+        "SELECT id, name, created FROM person
+        WHERE id = ANY( $1 )").unwrap();
+    let idOptions: Vec<Option<i32>> = ids.iter().map( |i| Some(*i) ).collect();
+    let idsForDb: postgres::types::array::ArrayBase<Option<i32>> =
+        postgres::types::array::ArrayBase::from_vec(idOptions, 0);
+    let mut iter = stmt.query(
+        [&idsForDb]).unwrap();
+    let mut persons: Vec<Person> = Vec::new();
+    for select in iter {
+        let person = Person {
+            id: select.get(0u),
+            name: select.get(1u),
+            created: select.get(2u)
+        };
+        persons.push(person);
+    }
+    let num_persons = persons.len();
+    if num_persons == 0 {
+        response.origin.status = http::status::NotFound;
+        let result = ServerMessage {
+            message: format!("{} persons were selected", num_persons)
+        };
+        let text = json::encode(&result);
+        response.send(text.as_slice());
+    }
+    else {
+        response.origin.status = http::status::Ok;
+        let result = GetPersonsMessage {
+            persons: persons
+        };
+        let text = json::encode(&result);
+        response.send(text.as_slice());
     }
 }
 
@@ -79,7 +181,7 @@ fn get_person (req: &Request, response: &mut Response) {
     let idStr: Option<int> = from_str(req.params.get(&"id".to_string()).as_slice());
     let id: i32 = idStr.unwrap() as i32;
     println!("get_person id={}", id);
-    let db_conn = req.map.find::<PooledPostgresConnection>().unwrap();
+    let db_conn = req.db_conn();
     let stmt = db_conn.prepare(
         "SELECT id, name, created FROM person WHERE id = $1").unwrap();
     let mut iter = stmt.query(
@@ -116,7 +218,7 @@ fn post_person(req: &Request, response: &mut Response) {
     set_response_content_type_json(response);
     println!("post_person called");
     let person: PersonByPost = req.json_as::<PersonByPost>().unwrap();
-    let db_conn = req.map.find::<PooledPostgresConnection>().unwrap();
+    let db_conn = req.db_conn();
     let inserts = db_conn.execute(
         "INSERT INTO person (name, created) VALUES ( $1, $2 )",
         [&person.name.as_slice(), &time::get_time()]).unwrap();
@@ -138,7 +240,7 @@ fn put_person(req: &Request, response: &mut Response) {
     set_response_content_type_json(response);
     println!("put_person called");
     let person: PersonByPut = req.json_as::<PersonByPut>().unwrap();
-    let db_conn = req.map.find::<PooledPostgresConnection>().unwrap();
+    let db_conn = req.db_conn();
     let updates = db_conn.execute(
         "UPDATE person SET ( name ) = ( $2 ) WHERE id = $1",
         [&person.id, &person.name.as_slice()]).unwrap();
@@ -161,7 +263,7 @@ fn delete_person (req: &Request, response: &mut Response) {
     let idStr: Option<int> = from_str(req.params.get(&"id".to_string()).as_slice());
     let id: i32 = idStr.unwrap() as i32;
     println!("delete_person id={}", id);
-    let db_conn = req.map.find::<PooledPostgresConnection>().unwrap();
+    let db_conn = req.db_conn();
     let deletes = db_conn.execute(
         "DELETE FROM person WHERE id = $1",
         [&id]).unwrap();
--- nickel-postgres/src/models.rs
+++ nickel-postgres/src/models.rs
@@ -3,6 +3,11 @@ extern crate serialize;
 use serialize::{ Decodable, Encodable };
 use time::Timespec;
 
+#[deriving(Encodable)]
+pub struct GetPersonsMessage {
+    pub persons: Vec<Person>
+}
+
 #[deriving(Decodable,Encodable)]
 pub struct Person {
     pub id: i32,
@@ -24,4 +29,18 @@ pub struct PersonByPut {
 #[deriving(Encodable)]
 pub struct ServerMessage {
     pub message: String
+}
+
+#[deriving(Decodable,Encodable)]
+pub struct Post {
+    pub id: i32,
+    pub title: String,
+    pub text: String
+}
+
+#[deriving(Decodable,Encodable)]
+pub struct Comment {
+    pub id: i32,
+    pub text: String,
+    pub post_id: String
 }
